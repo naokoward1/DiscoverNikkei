0afeb7d62af1e8a9c6566b8ea6c4c7f049283651
Smarter Sorting <naoko@smartersorting.com>
2022-09-26 17:13:38 -0700

    added a login function and a signup function

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles'
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from register import views as r_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('articles.urls'))
+    path('', include('articles.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    url(r'^signup/$', r_views.signup, name='signup'),
+
 ]
