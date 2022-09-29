217ef95974d93e9c19642b45a3bc486599710a02
Smarter Sorting <naoko@smartersorting.com>
2022-09-29 13:57:51 -0700

    added some minor changes

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
@@ -6,5 +7,8 @@ class Article(models.Model):
 	title = models.CharField(max_length = 100)
 	body = models.TextField()
 	source = models.TextField(default='None')
+	likes = models.ManyToManyField(User, related_name='article_like')
+	def number_of_likes(self):
+		return self.likes.count()
 	def __str__(self):
 		return self.title
